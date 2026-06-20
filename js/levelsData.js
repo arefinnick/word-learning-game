c9934ca6337eb35db04f240514225cbd1c3abb97
arefinnick <transfokator@bk.ru>
2026-06-20 16:22:26 +0500

    Замена аудиофайла i. Заполнение массива тема Тело.

--- js/levelsData.js
+++ js/levelsData.js
@@ -115,6 +115,101 @@ const levelsData = [
       { ru: "племянница", audioRu: "./assets/audio/03-family/niece-ru.mp3", en: "niece", audioEn: "./assets/audio/03-family/niece.mp3" },
       { ru: "двоюродный брат,сестра", audioRu: "./assets/audio/03-family/cousin-ru.mp3", en: "cousin", audioEn: "./assets/audio/03-family/cousin.mp3" }
     ]
+  },
+  {
+    id: 10,
+    theme: "Тело1",
+    number: "5",
+    words: [
+      { ru: "рука", audioRu: "./assets/audio/04-body/arm-ru.mp3", en: "arm", audioEn: "./assets/audio/04-body/arm.mp3" },
+      { ru: "спина", audioRu: "./assets/audio/04-body/back-ru.mp3", en: "back", audioEn: "./assets/audio/04-body/back.mp3" },
+      { ru: "борода", audioRu: "./assets/audio/04-body/beard-ru.mp3", en: "beard", audioEn: "./assets/audio/04-body/beard.mp3" },
+      { ru: "Тело", audioRu: "./assets/audio/04-body/body-ru.mp3", en: "body", audioEn: "./assets/audio/04-body/body.mp3" },
+      { ru: "кость", audioRu: "./assets/audio/04-body/bone-ru.mp3", en: "bone", audioEn: "./assets/audio/04-body/bone.mp3" }
+    ]
+  },
+  {
+    id: 11,
+    theme: "Тело2",
+    number: "5",
+    words: [
+      { ru: "ягодицы", audioRu: "./assets/audio/04-body/buttocks-ru.mp3", en: "buttocks", audioEn: "./assets/audio/04-body/buttocks.mp3" },
+      { ru: "щека", audioRu: "./assets/audio/04-body/cheek-ru.mp3", en: "cheek", audioEn: "./assets/audio/04-body/cheek.mp3" },
+      { ru: "грудь", audioRu: "./assets/audio/04-body/chest-ru.mp3", en: "chest", audioEn: "./assets/audio/04-body/chest.mp3" },
+      { ru: "подбородок", audioRu: "./assets/audio/04-body/chin-ru.mp3", en: "chin", audioEn: "./assets/audio/04-body/chin.mp3" },
+      { ru: "ухо", audioRu: "./assets/audio/04-body/ear-ru.mp3", en: "ear", audioEn: "./assets/audio/04-body/ear.mp3" }
+    ]
+  },
+  {
+    id: 12,
+    theme: "Тело3",
+    number: "5",
+    words: [
+      { ru: "локоть", audioRu: "./assets/audio/04-body/elbow-ru.mp3", en: "elbow", audioEn: "./assets/audio/04-body/elbow.mp3" },
+      { ru: "глаз", audioRu: "./assets/audio/04-body/eye-ru.mp3", en: "eye", audioEn: "./assets/audio/04-body/eye.mp3" },
+      { ru: "бровь", audioRu: "./assets/audio/04-body/eyebrow-ru.mp3", en: "eyebrow", audioEn: "./assets/audio/04-body/eyebrow.mp3" },
+      { ru: "лицо", audioRu: "./assets/audio/04-body/face-ru.mp3", en: "face", audioEn: "./assets/audio/04-body/face.mp3" },
+      { ru: "палец на руке", audioRu: "./assets/audio/04-body/finger-ru.mp3", en: "finger", audioEn: "./assets/audio/04-body/finger.mp3" }
+    ]
+  },
+  {
+    id: 13,
+    theme: "Тело4",
+    number: "5",
+    words: [
+      { ru: "ступня", audioRu: "./assets/audio/04-body/foot-ru.mp3", en: "foot", audioEn: "./assets/audio/04-body/foot.mp3" },
+      { ru: "лоб", audioRu: "./assets/audio/04-body/forehead-ru.mp3", en: "forehead", audioEn: "./assets/audio/04-body/forehead.mp3" },
+      { ru: "волосы", audioRu: "./assets/audio/04-body/hair-ru.mp3", en: "hair", audioEn: "./assets/audio/04-body/hair.mp3" },
+      { ru: "кисть руки", audioRu: "./assets/audio/04-body/hand-ru.mp3", en: "hand", audioEn: "./assets/audio/04-body/hand.mp3" },
+      { ru: "голова", audioRu: "./assets/audio/04-body/head-ru.mp3", en: "head", audioEn: "./assets/audio/04-body/head.mp3" }
+    ]
+  },
+  {
+    id: 14,
+    theme: "Тело5",
+    number: "5",
+    words: [
+      { ru: "сердце", audioRu: "./assets/audio/04-body/heart-ru.mp3", en: "heart", audioEn: "./assets/audio/04-body/heart.mp3" },
+      { ru: "пятка", audioRu: "./assets/audio/04-body/heel-ru.mp3", en: "heel", audioEn: "./assets/audio/04-body/heel.mp3" },
+      { ru: "колено", audioRu: "./assets/audio/04-body/knee-ru.mp3", en: "knee", audioEn: "./assets/audio/04-body/knee.mp3" },
+      { ru: "нога", audioRu: "./assets/audio/04-body/leg-ru.mp3", en: "leg", audioEn: "./assets/audio/04-body/leg.mp3" },
+      { ru: "губы", audioRu: "./assets/audio/04-body/lips-ru.mp3", en: "lips", audioEn: "./assets/audio/04-body/lips.mp3" }
+    ]
+  },
+  {
+    id: 15,
+    theme: "Тело6",
+    number: "5",
+    words: [
+      { ru: "рот", audioRu: "./assets/audio/04-body/mouth-ru.mp3", en: "mouth", audioEn: "./assets/audio/04-body/mouth.mp3" },
+      { ru: "усы", audioRu: "./assets/audio/04-body/mustache-ru.mp3", en: "mustache", audioEn: "./assets/audio/04-body/mustache.mp3" },
+      { ru: "ноготь", audioRu: "./assets/audio/04-body/nail-ru.mp3", en: "nail", audioEn: "./assets/audio/04-body/nail.mp3" },
+      { ru: "шея", audioRu: "./assets/audio/04-body/neck-ru.mp3", en: "neck", audioEn: "./assets/audio/04-body/neck.mp3" },
+      { ru: "нос", audioRu: "./assets/audio/04-body/nose-ru.mp3", en: "nose", audioEn: "./assets/audio/04-body/nose.mp3" }
+    ]
+  },
+  {
+    id: 16,
+    theme: "Тело7",
+    number: "5",
+    words: [
+      { ru: "орган", audioRu: "./assets/audio/04-body/organ-ru.mp3", en: "organ", audioEn: "./assets/audio/04-body/organ.mp3" },
+      { ru: "плечо", audioRu: "./assets/audio/04-body/shoulder-ru.mp3", en: "shoulder", audioEn: "./assets/audio/04-body/shoulder.mp3" },
+      { ru: "кожа", audioRu: "./assets/audio/04-body/skin-ru.mp3", en: "skin", audioEn: "./assets/audio/04-body/skin.mp3" },
+      { ru: "позвоночник", audioRu: "./assets/audio/04-body/spine-ru.mp3", en: "spine", audioEn: "./assets/audio/04-body/spine.mp3" },
+      { ru: "живот,желудок", audioRu: "./assets/audio/04-body/stomach-ru.mp3", en: "stomach", audioEn: "./assets/audio/04-body/stomach.mp3" }
+    ]
+  },
+  {
+    id: 17,
+    theme: "Тело8",
+    number: "4",
+    words: [
+      { ru: "горло", audioRu: "./assets/audio/04-body/throat-ru.mp3", en: "throat", audioEn: "./assets/audio/04-body/throat.mp3" },
+      { ru: "палец на ноге", audioRu: "./assets/audio/04-body/toe-ru.mp3", en: "toe", audioEn: "./assets/audio/04-body/toe.mp3" },
+      { ru: "язык", audioRu: "./assets/audio/04-body/tongue-ru.mp3", en: "tongue", audioEn: "./assets/audio/04-body/tongue.mp3" },
+      { ru: "зуб", audioRu: "./assets/audio/04-body/tooth-ru.mp3", en: "tooth", audioEn: "./assets/audio/04-body/tooth.mp3" }
+    ]
   }
   // ... остальные уровни
 ];
